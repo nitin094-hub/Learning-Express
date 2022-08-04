a97a17b2d86f7c3648f11f5e711772e360934de3
Nitin <nitinrajesh094@gmail.com>
2022-08-04 18:46:39 +0530

    changed schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,6 +5,18 @@ const tourScheme = new mongoose.Schema({
     required: [true, "A tour must have a name"],
     unique: true,
   },
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  maxGrpSize: {
+    type: Number,
+    required: [true, "A tour must have a group size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty"],
+  },
   rating: {
     type: Number,
     default: 4.5,
